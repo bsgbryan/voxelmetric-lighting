8f51b9b98a1297d146918d0ad7cdc17d54e71299
bsgbryan <bsgbryan@gmail.com>
2019-08-16 16:44:08 -0500

    Added boilerplate Render Pipeline code

--- Assets/Scripts/VoxelmetricLightingRP.cs
+++ Assets/Scripts/VoxelmetricLightingRP.cs
@@ -3,7 +3,32 @@ using UnityEngine.Rendering;
 
 public class VoxelmetricLightingRP : RenderPipeline {
 
-  protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
-    // Do something
+  private CommandBuffer commandBuffer = new CommandBuffer {
+    name = "Voxelmetric Lighting"
+  };
+
+  private CullingResults cullingResults;
+
+  public VoxelmetricLightingRP() {
+    GraphicsSettings.lightsUseLinearIntensity = true;
+  }
+
+  protected override void Render(ScriptableRenderContext renderContext, Camera[] cameras) {
+    ScriptableCullingParameters cullingParameters;
+
+    if (!cameras[0].TryGetCullingParameters(out cullingParameters))
+      return;
+
+    cullingResults = renderContext.Cull(ref cullingParameters);
+
+    renderContext.SetupCameraProperties(cameras[0]);
+
+    commandBuffer.ClearRenderTarget(true, false, Color.clear);
+
+    renderContext.ExecuteCommandBuffer(commandBuffer);
+
+    commandBuffer.Clear();
+
+    renderContext.Submit();
   }
 }
