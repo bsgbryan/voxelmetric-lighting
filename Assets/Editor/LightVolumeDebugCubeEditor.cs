512fabe485d3966d5db97159f368fee4de633728
bsgbryan <bsgbryan@gmail.com>
2019-08-20 10:43:19 -0500

    Whitespace fixes to match coding style

--- Assets/Editor/LightVolumeDebugCubeEditor.cs
+++ Assets/Editor/LightVolumeDebugCubeEditor.cs
@@ -3,23 +3,21 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(LightVolumeDebugCube))]
-public class LightVolumeDebugCubeEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+public class LightVolumeDebugCubeEditor : Editor {
+  public override void OnInspectorGUI() {
+    DrawDefaultInspector();
 
-        LightVolumeDebugCube debugCube = (LightVolumeDebugCube)target;
-        
-        if(GUILayout.Button("Create Debug Voxels") && debugCube.ShadowVoxels.Count == 0)
-          debugCube.CreateDebugVoxels();
+    LightVolumeDebugCube debugCube = (LightVolumeDebugCube) target;
+    
+    if(GUILayout.Button("Create Debug Voxels") && debugCube.ShadowVoxels.Count == 0)
+      debugCube.CreateDebugVoxels();
 
-        if(GUILayout.Button("Destroy Debug Voxels"))
-          debugCube.DestroyDebugVoxels();
+    if(GUILayout.Button("Destroy Debug Voxels"))
+      debugCube.DestroyDebugVoxels();
 
-        if(GUILayout.Button("Refresh Debug Voxels")) {
-          debugCube.DestroyDebugVoxels();
-          debugCube.CreateDebugVoxels();
-        }
+    if(GUILayout.Button("Refresh Debug Voxels")) {
+      debugCube.DestroyDebugVoxels();
+      debugCube.CreateDebugVoxels();
     }
+  }
 }
