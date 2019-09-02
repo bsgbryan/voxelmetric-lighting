174ac52800f00073cbfdebb6e0688b7ec7bf2b58
bsgbryan <bsgbryan@gmail.com>
2019-09-01 20:27:32 -0500

    Added regions, implemented vertex smoothing support

--- Assets/Scripts/LightVolumeDebugCube.cs
+++ Assets/Scripts/LightVolumeDebugCube.cs
@@ -6,39 +6,49 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class LightVolumeDebugCube : MonoBehaviour {
 
-  [Header("Details")]
-  public GameObject ShadowCaster;
-  [Range(.01f, 10f)] public float Resolution = 2f;
-  [Range(10f, 100f)] public float ShadowLength = 10f;
-  [Range(1f, 1.5f)] public float RayAugment = 1.1f;
-  [Range(2, 100)] public int MaxPasses = 10;
+  #region Editor properties
+    [Header("Details")]
+    public GameObject ShadowCaster;
+    [Range(.01f, 10f)] public float Resolution = 2f;
+    [Range(.1f, 10f)] public float Density = 1f;
+    public bool Smooth = false;
+    [Range(10f, 100f)] public float ShadowLength = 10f;
+    [Range(1f, 1.5f)] public float RayAugment = 1.1f;
+    [Range(2, 100)] public int MaxPasses = 10;
+    public Material MeshMaterial;
 
-  [Header("Layer Mask")]
-  public LayerMask Name;
-  public bool Ignore = true;
-  
-  [Header("Debug")]
-  public bool DrawInitialRay = true;
-  public Color InitialRayColor = Color.white;
-  public bool DrawHitRays = true;
-  [Range(.5f, 1f)] public float HitAlpha = .75f;
-  public bool DrawMissRays = false;
-  [Range(0f, .5f)] public float MissAlpha = .25f;
-  public bool DrawBoundingRays = true;
-  public Color BoundingRayColor = Color.black;
-  [Range(.25f, 1f)] public float Red = .5f;
-  [Range(.25f, 1f)] public float Green = .5f;
-  [Range(.25f, 1f)] public float Blue = .5f;
+    [Header("Layer Mask")]
+    public LayerMask Name;
+    public bool Ignore = true;
 
-  private GameObject ShadowVolume;
+    [Header("Debug")]
+    public bool DrawInitialRay = true;
+    public Color InitialRayColor = Color.white;
+    public bool DrawHitRays = true;
+    [Range(.5f, 1f)] public float HitAlpha = .75f;
+    public bool DrawMissRays = false;
+    [Range(0f, .5f)] public float MissAlpha = .25f;
+    public bool DrawBoundingRays = true;
+    public Color BoundingRayColor = Color.black;
+    public bool DrawShadowMesh = true;
+    [Range(.25f, 1f)] public float Red = .5f;
+    [Range(.25f, 1f)] public float Green = .5f;
+    [Range(.25f, 1f)] public float Blue = .5f;
+  #endregion
 
-  [HideInInspector] public Vector3[] ShadowVertices;
-  [HideInInspector] public int[] ShadowTriangles;
-  [HideInInspector] public int BoundingRayCount = 0;
-  [HideInInspector] public int Hits = 0;
-  [HideInInspector] public int Misses = 0;
-  [HideInInspector] public int PassesExecuted = 0;
-  [HideInInspector] public int UnneededPasses = 0;
+  #region Hidden properties
+    [HideInInspector] public Vector3[] ShadowVertices;
+    [HideInInspector] public int[] ShadowTriangles;
+    [HideInInspector] public int BoundingRayCount = 0;
+    [HideInInspector] public int Hits = 0;
+    [HideInInspector] public int Misses = 0;
+    [HideInInspector] public int PassesExecuted = 0;
+    [HideInInspector] public int UnneededPasses = 0;
+  #endregion
+
+  #region Private properties
+    private GameObject ShadowVolume;
+  #endregion
 
   private void Update() => CastShadows();
 
@@ -95,15 +105,18 @@ public class LightVolumeDebugCube : MonoBehaviour {
           g = Green;
         
         for (int step = -passes; step < passes; step++) {
-          int x = processingPositive ? passes : -passes;
-          int y = processingPositive ? step   : -step;
+          float x = (float) (processingPositive ? passes : -passes);
+          float y = (float) (processingPositive ? step   : -step);
 
           if (processingYAxis) {
-            y = processingPositive ?  passes : -passes;
-            x = processingPositive ? -step   :  step;
+            y = (float) (processingPositive ?  passes : -passes);
+            x = (float) (processingPositive ? -step   :  step);
           }
 
-          Vector3 position  = p + transform.rotation * new Vector3(x * scale, y * scale, 0f);
+          x *= 1f / Density;
+          y *= 1f / Density;
+
+          Vector3 position  = p + transform.rotation * new Vector3(x * scale, y * scale , 0f);
           Vector3 direction = transform.TransformDirection(Vector3.back);
 
           RaycastHit hitInfo;
@@ -151,8 +164,51 @@ public class LightVolumeDebugCube : MonoBehaviour {
       bool even = i % 2 == 0;
       int  key  = pointKeys[i];
 
-      ShadowVertices[vertexIndex++] = shadowPoints[key];
-      ShadowVertices[vertexIndex++] = shadowPoints[key] + (transform.TransformDirection(Vector3.back) * shadowLengths[key]);
+      if (Smooth == true) {
+        int prevPrevKey = 0;
+        int previousKey = 0;
+        int nextKey     = 0;
+        int nextNextKey = 0;
+
+        if (i == 0) {
+          prevPrevKey = pointKeys[pointKeys.Count - 2];
+          previousKey = pointKeys.Last();
+          nextKey     = pointKeys[1];
+          nextNextKey = pointKeys[2];
+        } else if (i == 1) {
+          prevPrevKey = pointKeys.Last();
+          previousKey = pointKeys.First();
+          nextKey     = pointKeys[2];
+          nextNextKey = pointKeys[3];
+        } else if (i == pointKeys.Count - 1) {
+          prevPrevKey = pointKeys[i - 2];
+          previousKey = pointKeys[i - 1];
+          nextKey     = pointKeys.First();
+          nextNextKey = pointKeys[1];
+        } else if (i == pointKeys.Count - 2) {
+          prevPrevKey = pointKeys[pointKeys.Count - 3];
+          previousKey = pointKeys[pointKeys.Count - 2];
+          nextKey     = pointKeys.Last();
+          nextNextKey = pointKeys.First();
+        } else {
+          prevPrevKey = pointKeys[i - 2];
+          previousKey = pointKeys[i - 1];
+          nextKey     = pointKeys[i + 1];
+          nextNextKey = pointKeys[i + 2];
+        }
+
+        Vector3 prevPrev = shadowPoints[prevPrevKey];
+        Vector3 previous = shadowPoints[previousKey];
+        Vector3 next     = shadowPoints[nextKey];
+        Vector3 nextNext = shadowPoints[nextNextKey];
+        Vector3 averaged = (prevPrev + previous + shadowPoints[key] + next + nextNext) / 5;
+
+        ShadowVertices[vertexIndex++] = averaged;
+        ShadowVertices[vertexIndex++] = averaged + (transform.TransformDirection(Vector3.back) * shadowLengths[key]);
+      } else {
+        ShadowVertices[vertexIndex++] = shadowPoints[key];
+        ShadowVertices[vertexIndex++] = shadowPoints[key] + (transform.TransformDirection(Vector3.back) * shadowLengths[key]);
+      }
 
       if (even) {
         if (i >= 2) {
@@ -192,14 +248,18 @@ public class LightVolumeDebugCube : MonoBehaviour {
       ShadowVolume.GetComponent<BoxCollider>().enabled = false;
     }
 
-    var shadowMesh = new Mesh {
-      vertices  = (Vector3[]) ShadowVertices.Clone(),
-      triangles = (int[]) ShadowTriangles.Clone()
-    };
+    if (DrawShadowMesh) {
+      var shadowMesh = new Mesh {
+        vertices  = (Vector3[]) ShadowVertices.Clone(),
+        triangles = (int[]) ShadowTriangles.Clone()
+      };
 
-    shadowMesh.RecalculateNormals();
+      shadowMesh.RecalculateNormals();
 
-    ShadowVolume.GetComponent<MeshFilter>().sharedMesh = shadowMesh;
+      ShadowVolume.GetComponent<MeshRenderer>().sharedMaterial = MeshMaterial;
+      ShadowVolume.GetComponent<MeshFilter>().sharedMesh       = shadowMesh;
+    } else
+      ShadowVolume.GetComponent<MeshFilter>().sharedMesh = null;
 
     if (DrawBoundingRays)
       for(int i = 0; i < pointKeys.Count; i++)
